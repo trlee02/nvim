2bb5ff13244e55b4a96c4945152900b7dac57bff
Tristan Lee <tristan.rene.lee@gmail.com>
2024-03-17 01:32:06 -0700

    which-key

--- lua/tristan/packer.lua
+++ lua/tristan/packer.lua
@@ -15,6 +15,41 @@ return require('packer').startup(function(use)
 			vim.cmd('colorscheme fluoromachine')
 		end
 	})
-	ouse('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('ThePrimeagen/harpoon')
+	use {
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup { }
+		end
+	}
+	use('mbbill/undotree')
+	use("tpope/vim-fugitive")
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'hrsh7th/cmp-nvim-lua'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'}
+		}
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
