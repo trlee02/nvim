250007c6afaee1aba9406393818ff8a8a1e9118e
Tristan Lee <tristan.rene.lee@gmail.com>
2024-03-17 01:31:36 -0700

    added fluoromachine config

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,12 @@
 function ColorMyPencils(color)
-	color = color or "fluoromachine"
-	vim.cmd.colorscheme(color)
+    local fm = require('fluoromachine')
+
+    fm.setup {
+        brightness = 0.1,
+        transparent = 'false',
+    }
+
+    vim.cmd.colorscheme('fluoromachine')
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
