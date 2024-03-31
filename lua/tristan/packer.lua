0104fc8c2d4ad559dd09f2da07a394b7dff50c8c
Tristan Lee <tristan.rene.lee@gmail.com>
2024-03-31 01:03:46 -0700

    copilot

--- lua/tristan/packer.lua
+++ lua/tristan/packer.lua
@@ -52,4 +52,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use('github/copilot.vim')
 end)
