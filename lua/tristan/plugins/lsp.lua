5af957ec4ced6fb48009f0435800c062703dcc3c
Tristan Lee <tristan.rene.lee@gmail.com>
2024-06-03 12:33:11 -0700

    clangd

--- lua/tristan/plugins/lsp.lua
+++ lua/tristan/plugins/lsp.lua
@@ -11,33 +11,51 @@ return {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
+        "folke/neodev.nvim",
     },
 
     config = function()
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
 
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+        local servers = {
+            clangd = {
+                settings = {
+                    "clangd",
+                    "--suggest-missing-includes",
+                    "--background-index",
+                },
+            },
+            lua = {
+                settings = {
+                    Lua = {
+                        runtime = { version = "Lua 5.1" },
+                        diagnostics = {
+                            globals = { "vim", "it", "describe", "before_each", "after_each" },
+                        }
+                    }
+                }
+            }
+        }
+
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "clangd",
-            },
+            ensure_installed = vim.tbl_keys(servers),
             handlers = {
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
+                 function(server_name) -- default handler (optional)
+
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+                function(server_name)
+                    require('lspconfig')[server_name].setup {
                         capabilities = capabilities,
-                        settings = {
-                            Lua = {
-				    runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
+                        settings = servers[server_name].settings,
                     }
                 end,
             }
@@ -81,64 +99,3 @@ return {
         })
     end
 }
-
---    local opts = {buffer = bufnr, remap = false}
---    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
---    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
---    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
---    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
---    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
---    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
---    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
---    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
---
---    -- to learn how to use mason.nvim with lsp-zero
---    -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
---    require('mason').setup({})
---    require('mason-lspconfig').setup({
---        ensure_installed = {'clangd', 'lua_ls'},
---        handlers = {
---            lsp_zero.default_setup,
---            lua_ls = function()
---                local lua_opts = lsp_zero.nvim_lua_ls()
---                require('lspconfig').lua_ls.setup(lua_opts)
---            end,
---            clangd = function()
---                local clangd_opts = lsp_zero.preset('clangd')
---                clangd_opts.cmd = {
---                    "clangd",
---                    "--offset-encoding=utf-16",
---                    "--background-index",
---                    "--suggest-missing-includes"
---                }
---                require('lspconfig').clangd.setup(clangd_opts)
---            end,
---        }
---    })
---
---    local cmp = require('cmp')
---    local cmp_select = {behavior = cmp.SelectBehavior.Select}
---
---    -- this is the function that loads the extra snippets to luasnip
---    -- from rafamadriz/friendly-snippets
---    require('luasnip.loaders.from_vscode').lazy_load()
---
---    cmp.setup({
---        sources = {
---            {name = 'path'},
---            {name = 'nvim_lsp'},
---            {name = 'nvim_lua'},
---            {name = 'luasnip', keyword_length = 2},
---            {name = 'buffer', keyword_length = 3},
---        },
---        formatting = lsp_zero.cmp_format({details = false}),
---        mapping = cmp.mapping.preset.insert({
---            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
---            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
---            ['<C-Space>'] = cmp.mapping.complete(),
---        }),
---    })
-
