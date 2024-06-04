62a552d3f7d2680d766da14fc09b5b0b017769d2
Tristan Lee <tristan.rene.lee@gmail.com>
2024-06-04 15:19:56 -0700

    changed reference mapping

--- lua/tristan/init.lua
+++ lua/tristan/init.lua
@@ -2,13 +2,7 @@ require("tristan.set")
 require("tristan.remap")
 require("tristan.lazy_init")
 
-
-local augroup = vim.api.nvim_create_augroup
-local tristan_group = augroup('tristan', {})
-
-local autocmd = vim.api.nvim_create_autocmd
-autocmd('LspAttach', {
-    group = tristan_group,
+vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -16,7 +10,7 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
