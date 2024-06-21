a42dde0ea6b5e96f3756beb84142e4a44f74cacb
Tristan Lee <tristan.rene.lee@gmail.com>
2024-06-20 17:06:33 -0700

    fzf telescipe fix

--- lua/tristan/plugins/telescope.lua
+++ lua/tristan/plugins/telescope.lua
@@ -1,11 +1,7 @@
 return {
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
 
         dependencies = {
             'nvim-lua/plenary.nvim'
