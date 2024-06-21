97ded131f71498f6f46400efba0ee753ed1638b6
Tristan Lee <tristan.rene.lee@gmail.com>
2024-06-21 11:12:24 -0700

    fixed colors

---
+++ lua/tristan/plugins/fluoromachine.lua
@@ -0,0 +1,19 @@
+
+return {
+    {
+        'maxmx03/fluoromachine.nvim',
+        config = function()
+            local fm = require('fluoromachine')
+            fm.setup ({
+                glow = false,
+                theme = 'fluoromachine',
+                transparent = 'full',
+            })
+
+            vim.cmd.colorscheme('fluoromachine')
+        end
+    }
+}
+
+
+
