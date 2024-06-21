ab19832b3efc7dadf7bd54f279beb9e10aea79b3
Tristan Lee <tristan.rene.lee@gmail.com>
2024-06-21 11:12:49 -0700

    changed lsp accept

--- lua/tristan/plugins/firenvim.lua
+++ lua/tristan/plugins/firenvim.lua
@@ -7,13 +7,5 @@ return {
     build = function()
         vim.fn["firenvim#install"](0)
     end,
-    vim.api.nvim_create_autocmd({'UIEnter'}, {
-        callback = function(event)
-            local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
-            if client ~= nil and client.name == "Firenvim" then
-                vim.o.laststatus = 0
-            end
-        end
-    })
 }
 
